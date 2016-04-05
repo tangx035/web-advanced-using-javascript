ad1ecf19341c823676084d53b0d6e617d9dfd66d
Saman Tehrani <samanrtehrani@gmail.com>
2016-04-05 13:03:21 -0400

    modal panel added

--- assignments/2/dev/scripts/main.js
+++ assignments/2/dev/scripts/main.js
@@ -1,4 +1,18 @@
+/*jslint browser: true, plusplus: true */
+/*eslint-disable no-console */
+/*eslint-disable indent */
+/*eslint-disable no-mixed-spaces-and-tabs */
+/*global window, document */
 window.onload = function () {
     'use strict';
-
+    document.onkeydown = function(event){
+    	if( 27 === event.keyCode ){
+    		var modalPanel = document.querySelector('.modal-panel');
+    		if( modalPanel.style.display == 'none')
+    			modalPanel.style.display = 'block';
+    		else
+    			modalPanel.style.display = 'none';
+    		
+    	}
+    };
 };
