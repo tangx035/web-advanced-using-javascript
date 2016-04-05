dc4ba4c517b7e71bd4f103937c22873f46dd3150
Alonso <casta828@newschool.edu>
2016-04-05 12:46:22 -0400

    modal working

--- assignments/2/dev/scripts/main.js
+++ assignments/2/dev/scripts/main.js
@@ -1,4 +1,45 @@
 window.onload = function () {
     'use strict';
 
+    //declaring html body
+    var body = document.getElementsByTagName('body')[0];
+
+    //modal transparency...check css
+
+    var myModal = document.createElement('div');
+    myModal.id = 'myModal';
+    body.appendChild(myModal);
+
+
+    //creating the modal
+    var modalFront = document.createElement('div');
+    modalFront.id = 'modalFront';
+    modalFront.textContent = 'My modal.';
+    body.appendChild(modalFront);
+
+    //popup modal
+    var modalTextButton = document.getElementsByTagName('li')[9];
+    function showModal(){
+        document.getElementById('myModal').style.display = 'block';
+        document.getElementById('modalFront').style.display = 'block';
+    }
+    modalTextButton.addEventListener('click', showModal, false);
+
+    //exit
+    function exitModal() {
+        document.getElementById('myModal').style.display = 'none';
+        document.getElementById('modalFront').style.display = 'none';
+    }
+    myModal.addEventListener('click', exitModal, false);
+
+    //menu show and hide
+    var menu = document.getElementsByTagName('li')[8];
+    function showMenu(){
+        document.getElementsByTagName('ul')[0].style.visibility = 'visible';
+        document.getElementsByTagName('ul')[0].style.top = '0';
+    }
+    menu.addEventListener('click', showMenu, false);
+
+    function hiddenMenu(){}
+
 };
