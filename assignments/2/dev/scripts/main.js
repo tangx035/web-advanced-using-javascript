6ca305fbb927f789032a9303a7a93f9af94fbe01
Saman Tehrani <samanrtehrani@gmail.com>
2016-04-05 14:52:10 -0400

    modal button trigger added, also out of modal content click trigger added

--- assignments/2/dev/scripts/main.js
+++ assignments/2/dev/scripts/main.js
@@ -6,40 +6,54 @@
 /*global window, document */
 window.onload = function () {
     'use strict';
+
+
+    var modalButton = document.querySelector('#js-triggers li:last-of-type a');
+    modalButton.addEventListener('click',function(e){
+        e.preventDefault();
+        toggleModal();
+    },false);
+
+
     document.onkeydown = function(event){
     	if( 27 === event.keyCode ){
-    		var modalPanel = document.querySelector('.modal-panel');
+            toggleModal();
+    	}
+    };
+    var toggleModal = function(state){
+        if( !state ){
+            var modalPanel = document.querySelector('.modal-panel');
             if( modalPanel.style.display === ''){
                 modalPanel.style.display = 'none';
             }
-    		var modalPanelContent = modalPanel.querySelector('.modal-content');
+            var modalPanelContent = modalPanel.querySelector('.modal-content');
 
             // to avoid duplicates of click handler, remove them everytime 
             // that new one wants to get added
             modalPanelContent.removeEventListener('click',modalPanelContentClickHandler);
-    		modalPanelContent.addEventListener('click',modalPanelContentClickHandler,false);
+            modalPanelContent.addEventListener('click',modalPanelContentClickHandler,false);
             modalPanel.removeEventListener('click',modalPanelClickHandler);
-    		modalPanel.addEventListener('click',modalPanelClickHandler,false);
+            modalPanel.addEventListener('click',modalPanelClickHandler,false);
 
-    		if( modalPanel.style.display == 'none'){
+            if( modalPanel.style.display == 'none'){
                 //disbale body scroll temporarily
-    			document.body.classList.add('modal-panel-is-open');
-    			modalPanel.style.display = 'flex';
-    		}else{
+                document.body.classList.add('modal-panel-is-open');
+                modalPanel.style.display = 'flex';
+            }else{
                 //enable body scroll
-    			document.body.classList.remove('modal-panel-is-open');
-    			modalPanel.style.display = 'none';
-    		}
-    		
-    	}
+                document.body.classList.remove('modal-panel-is-open');
+                modalPanel.style.display = 'none';
+            }
+        }
+
     };
     var modalPanelContentClickHandler= function(e){
         e.stopPropagation();
-        console.log('IN');
+        
     };
     var modalPanelClickHandler= function(e){
         e.stopPropagation();
-        console.log('OUT');
+        toggleModal();        
     };
 };
 
