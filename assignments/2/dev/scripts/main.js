d62da8ef290d9297a8745a4e7a761208ce435be0
Alonso <casta828@newschool.edu>
2016-04-06 14:07:45 -0400

    no sidetray

--- assignments/2/dev/scripts/main.js
+++ assignments/2/dev/scripts/main.js
@@ -5,11 +5,9 @@ window.onload = function () {
     var body = document.getElementsByTagName('body')[0];
 
     //modal transparency...check css
-
     var myModal = document.createElement('div');
     myModal.id = 'myModal';
     body.appendChild(myModal);
-
 
     //creating the modal
     var modalFront = document.createElement('div');
@@ -21,28 +19,49 @@ window.onload = function () {
     var modalTextButton = document.getElementsByTagName('li')[9];
     function showModal(){
         document.getElementById('myModal').style.display = 'block';
+        document.getElementById('myModal').style.transitionDuration = '500ms';
         document.getElementById('modalFront').style.display = 'block';
+        document.getElementById('modalFront').style.transitionDuration = '500ms';
     }
     modalTextButton.addEventListener('click', showModal, false);
 
     //exit
     function exitModal() {
         document.getElementById('myModal').style.display = 'none';
+        document.getElementById('myModal').style.transitionDuration = '500ms';
         document.getElementById('modalFront').style.display = 'none';
+        document.getElementById('modalFront').style.transitionDuration = '500ms';
     }
     myModal.addEventListener('click', exitModal, false);
 
-    //menu show and hide!
-    var menu = document.getElementsByTagName('li')[8];
-    function showMenu(){
-        document.getElementsByTagName('ul')[0].style.visibility = 'visible';
-        document.getElementsByTagName('ul')[0].style.top = '0';
-    }
-    menu.addEventListener('click', showMenu, false);
+	// menu show and hide
+    var myMenu = document.getElementsByTagName('ul')[0];
+    var menuTextButton = document.getElementsByTagName('li')[8];
 
-    function hiddenMenu(){
-        document.getElementsByTagName('ul')[0].style.visibility = 'visible';
-    }
-    menu.addEventListener('click', hiddenMenu, false);
+    menuTextButton.onclick = function (){
+        if (menuTextButton.style.visibility !== 'visible'){
+            myMenu.style.visibility = 'visible';
+            myMenu.style.top = '0';
 
+            if (window.innerWidth <= 736){
+                myMenu.classList.add('reveal-nav');
+                window.addEventListener('resize', function(){
+                    window.location.reload();
+                });
+            }
+        } else {
+            myMenu.style.visibility = 'hidden';
+            myMenu.style.transitionDuration = '500ms';
+        }          
+    };
+
+    menuTextButton.onmouseout = function(){
+        myMenu.style.visibility = 'hidden';
+
+    };
 };
+
+
+
+
+
