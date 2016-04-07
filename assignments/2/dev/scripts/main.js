8c7f38e258335b0532190add23d902d66c4582e0
Jasmine(Woo Jung) Oh <ohw979@newschool.edu>
2016-04-06 23:19:15 -0400

    Gulp built

--- assignments/2/dev/scripts/main.js
+++ assignments/2/dev/scripts/main.js
@@ -15,10 +15,6 @@ window.onload = function () {
     body.appendChild(modalBox);
 
     var modalButton = document.getElementsByTagName('li')[9];
-    
-    // //EXIT BUTTON
-    // var exitButton= document.createElement('div');
-    // exitButton.textContent = 'X';
 
     //OPEN MODAL BOX FUNCTION
     function openModal () {
@@ -41,4 +37,99 @@ window.onload = function () {
             exitModal();
         }
     };
+
+    //MENU
+    var showMenuButton = document.getElementsByTagName('li')[8];
+    var menu = document.getElementsByTagName('ul')[0];
+    showMenuButton.onclick = function() {
+        if (menu.style.visibility !== 'visible') {
+            showMenu();
+            if (window.innerWidth <= 736) {
+                menu.classList.add('showSideTray');
+                window.addEventListener('resize', function() { 
+                    window.location.reload(); 
+                });
+            }
+        }
+        else {
+            hideDropdown();
+            // menu.classList.add('hideSideTray');
+        }
+    };
+
+    function showMenu() {  
+        menu.style.visibility = 'visible';
+        menu.style.top = '0px';
+        menu.style.transition = 'left 500ms ease';
+        menu.style.position = 'absolute';
+    }
+
+    //HIDE DROPDOWN MENU
+    function hideDropdown(){
+        menu.style.visibility = 'hidden';
+        menu.style.position = 'absolute';
+        menu.style.top = '-500px';
+        menu.style.transition = 'all 500ms ease-in-out';
+    }
+
+    // function hideSideTray(){
+    //     menu.style.visibility = 'hidden';
+    //     showMenuButton.style.color = 'white';
+    //     menu.style.position = 'absolute';
+    //     menu.style.left = '-500px';
+    //     menu.style.transition = 'left 500ms ease';
+
+    // };
 };
+
+
+    //DROPDOWN WHEN SCREEN LARGER THAN 736-pixel
+    // var breakpoint = window.matchMedia('screen and (min-width: 736px)');
+    // if (breakpoint.matches) {
+    //     showMenuButton.addEventListener('click', showMenu, false);
+    // }
+
+    // if (matchMedia) {
+    //     var breakpoint = window.matchMedia('screen and (min-width: 736px)');
+    //     breakpoint.addListener(widthChange);
+    //     widthChange(breakpoint);
+    // }
+
+    // function widthChange(breakpoint) {
+    //     if (breakpoint.matches) {
+    //         menu.addEventListener('click', showMenu, false);
+    //     } else {
+    //         menu.style.display = 'none';
+    //     }
+    // }
+
+    //ASSIGN ID 
+    // menu.classList.add('menuClass');
+
+    // window.onresize = function(){
+    //     var width = window.innerWidth;
+    //     if (width <= 736){
+    //         menu.classList.add('menuClass');
+    //         showSideTray();
+    //     }else if (width > 736){
+    //         hideSidetray();
+    //         showMenu();
+    //     }
+    // };
+
+
+    //SIDETRAY WITH RESIZE
+  
+    // function showSideTray(){
+    //     menu.style.position = 'fixed';
+    //     menu.style.display = 'block';
+    //     menu.style.marginTop = '0px';
+    //     menu.style.marginLeft = '0px';
+    //     menu.style.transition = 'all 500ms ease-in-out';
+    // }
+
+    // function hideSidetray(){
+    //     menu.style.marginTop = '0px';
+    //     menu.style.marginLeft = '-200px';
+    //     menu.style.display = 'inline';
+    // }
